1849a9dd41901fe5607bd1cb6fb31bf6222c8248
Mattie Schraeder <mattie@csharpmarc.net>
2019-10-14 20:01:12 -0500

    A skeleton emerges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'boardgames',
+    loadChildren: () => import('./boardgames/boardgames.module').then(m => m.BoardgamesModule)
+  },
+  {
+    path: 'development',
+    loadChildren: () => import('./development/development.module').then(m => m.DevelopmentModule)
+  },
+  {
+    path: 'library-science',
+    loadChildren: () => import('./library-science/library-science.module').then(m => m.LibraryScienceModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
