270e633cffd40d6442c0feb7a03851a64a18f58c
Mattie Schraeder <mattie@mattie.lgbt>
2019-10-16 11:18:30 -0500

    Add geekway to game container

--- src/app/boardgames/game-container/game-container.component.ts
+++ src/app/boardgames/game-container/game-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Games, Game } from '../boardgames.types';
 
 @Component({
@@ -10,9 +11,21 @@ export class GameContainerComponent implements OnInit {
 
   games: Game[] = Games;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  redirect(url: string) {
+    if (url.startsWith("http")) {
+      this.router.navigate(['/externalRedirect', { externalUrl: url }], {
+        skipLocationChange: true,
+      });
+    } else {
+      this.router.navigate([url]);
+    }
+    
+    event.preventDefault();
   }
 
 }
